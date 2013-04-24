038ca1007258e401e5483fabf7354ba0e32b0a7a
Gerry Pass <rgpass@gmail.com>
2013-04-24 15:44:22 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,19 @@
+# This is a test page used for Test-Driven Development (TDD)
+# which is when you define your Tests first, which will
+# obviously fail the first time, then write code to make it
+# work out. To run this, use the Terminal prompt:
+# bundle exec rspec spec/requests/static_pages_spec.rb
+
+# Note: Some of the lines use single-quoted strings and others
+# use double-quoted. The only difference is that you cannot
+# interpolate single strings. "#{foo}"+"bar" => "foobar" but
+# '#{foo}'+'bar' will return "\#{foo} bar"
+# Benefit of single quotes is that it's literal and won't interpolate
+
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
@@ -9,8 +22,13 @@ describe "Static pages" do
     it "should have the right title" do
     	visit '/static_pages/home'
     	page.should have_selector('title', 
-    										:text => " | Home")
+    										:text => "Ruby on Rails Tutorial Sample App")
     end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
   describe "Help page" do
@@ -21,7 +39,7 @@ describe "Static pages" do
   	it "should have the title 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('title', 
-  											:text => " | Help")
+  											:text => "| Help")
   	end
   end
 
@@ -33,8 +51,20 @@ describe "Static pages" do
  		it "should have the title 'About Us'" do
  			visit '/static_pages/about'
  			page.should have_selector('title', 
- 												:text => " | About Us")
+ 												:text => "| About Us")
  		end
  	end
+
+  describe "Contact page" do
+    it "should have the content 'Conctact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+                        :text => "| Contact Us")
+    end
+  end
 
 end
